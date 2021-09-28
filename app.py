8124fe2de850dca83222cf3abec5c1e6fde5bfd5
krissykodes <kristinscholten@gmail.com>
2021-09-27 17:09:10 -0700

    updated

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-import numpy as np
+
 
 import sqlalchemy
 from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine, func
+import datetime as dt
 
 from flask import Flask, jsonify
 
@@ -21,6 +22,7 @@ Base.prepare(engine, reflect=True)
 # Save reference to the table
 measurement = Base.classes.measurement
 station = Base.classes.station
+session = Session(engine)
 
 #################################################
 # Flask Setup
@@ -37,8 +39,9 @@ def welcome():
     """List all available api routes."""
     return (
         f"Available Routes:<br/>"
-        f"/api/v1.0/names<br/>"
-        f"/api/v1.0/passengers"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"/api/v1.0/tobs<br/>"
     )
 
 
@@ -52,15 +55,15 @@ def precipitation():
     precipitation = session.query(measurement.date, measurement.prcp).\
     filter(measurement.date > last_12_months).\
     order_by(measurement.date).all()
-   
-    return jsonify(precipitation)
+    bananas=[    list(el)for el in precipitation]
+    return jsonify(bananas)
 
 
 @app.route("/api/v1.0/stations")
 def passengers():
     most_active = session.query(measurement.station, func.count(measurement.station)).group_by(measurement.station).order_by(func.count(measurement.station).desc()).all()
-
-    return jsonify(most_active)
+    bananas=[    list(el)for el in most_active]
+    return jsonify(bananas)
 
 # TOBS
 @app.route("/api/v1.0/tobs")
@@ -71,7 +74,8 @@ def tobs():
     station_temps = most_active[0][0]
 
     station_temps = session.query(func.min( measurement.tobs), func.max(measurement.tobs), func.avg(measurement.tobs)).filter(measurement.station == station_temps).all()
-
+    bananas=[    list(el)for el in station_temps]
+    return jsonify(bananas)
 
 if __name__ == '__main__':
     app.run(debug=True)
